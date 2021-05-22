3f38e80aec784cecfd8b1c95feb8e34acb9bba06
Vechtech <74859132+Vechtech@users.noreply.github.com>
2021-05-22 16:09:32 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+nose_x = 0;
+nose_y = 0;
+
+function preload(){
+    clown_nose = loadImage("https://i.postimg.cc/7ZBcjDqp/clownnose.png");
+}
+
+function setup(){
+    canvas = createCanvas(300,300);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(300,300);
+    video.hide();
+    poseNet = ml5.poseNet(video,modelloaded);
+    poseNet.on('pose',gotPoses);
+}
+
+function modelloaded(){
+    console.log("Model has loaded");
+}
+
+function gotPoses(results){
+    if(results.length > 0){
+        console.log(results);
+        nose_x = results[0].pose.nose.x - 15;
+        nose_y = results[0].pose.nose.y - 15;
+    }
+}
+
+function draw(){
+    image(video,0,0,300,300);
+    image(clown_nose,nose_x,nose_y,30,30)
+}
